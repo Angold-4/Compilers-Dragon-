dad6dfd1217c9268bba95f6084fce4486553aba5
Angold-4 <wangold4w@gmail.com>
2021-03-10 23:40:30 +0800

    fondEnd Compiler 20210309 A1-A4 20210310 A5-A6

---
+++ Chapters/Chapter2/fontEndCompiler/inter/Constant.java
@@ -0,0 +1,16 @@
+package lexer;
+import lexer.*;
+import symbols.*;
+public class Constant extends Expr {
+    public Constant(Token tok, Type p) { super(tok, p); }
+    public Constant(int i) { super(new Num(i), Type.Int); }
+
+    public static final Constant
+        True  = new Constant(Word.True, Type.Bool),
+        False = new Constant(Word.False, Type.Bool);
+
+    public void jumping(int t, int f) {
+        if ( this == True && t != 0 ) emit("goto L" + t);
+        else if ( this == false && f != 0 ) emit("goto L" + f);
+    }
+}
